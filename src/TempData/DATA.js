7ffb8519ad803f85d7f28f99df40cec32e491eed
Dan-McGrath <mcgrathd3254@yahoo.com>
2023-05-14 22:24:57 -0400

    make temp data

---
+++ src/TempData/DATA.js
@@ -0,0 +1,29 @@
+const DATA = {
+    profile: {
+        name: 'Dan',
+        playlist: ['playlist1', 'playlist2', 'playlist3'],
+        img: 'tempHeadshot.jpg',
+        alt: 'profile picture'
+    },
+
+    tracks: [
+        {
+            artist: 'artist1',
+            songName: 'songName1',
+            duration: '1:30' 
+        },
+        {
+            artist: 'artist2',
+            songName: ['songName1', 'songName2', 'songName3'],
+            duration: '2:30'
+        },
+        {
+            artist: 'artist3',
+            songName: 'songName3',
+            duration: '3:30'
+        }
+    ]
+            
+        
+
+}
