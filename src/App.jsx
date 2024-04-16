35123e0fba95ca53b62e24cc45086aa39d0e7b4c
Dan McGrath <mcgrathd3254@yahoo.com>
2024-04-16 19:42:52 -0400

    update styling. Add Playlist remove Tracklist

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import Playlist from "./components/Playlist";
 import SearchBar from "./components/SearchBar";
-import Tracklist from "./components/Tracklist";
+import SearchResults from "./components/SearchResults";
 
 function App() {
   return (
-    <div className="bg-background h-screen">
+    <div className="min-h-lvh">
       <header className="text-center mb-2 mx-auto bg-blue-gray py-4">
         <h1 className="text-3xl text-lite-green">
           Ja<span className="text-neutral-300">mmm</span>ing
         </h1>
       </header>
       <SearchBar />
-      <Playlist />
-      <Tracklist />
+      <div className="grid grid-cols-2 w-10/12 mx-auto gap-6 min-h-lvh">
+        <SearchResults />
+        <Playlist />
+      </div>
     </div>
   );
 }
