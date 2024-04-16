7024333f51af84c4152af2a49f39d3ccd2df0a5e
Dan McGrath <mcgrathd3254@yahoo.com>
2024-04-16 18:30:39 -0400

    Add base components and a header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import Playlist from "./components/Playlist";
+import SearchBar from "./components/SearchBar";
+import Tracklist from "./components/Tracklist";
+
 function App() {
   return (
-    <>
-      <h1 className="">Hello World</h1>
-    </>
+    <div className="bg-background h-screen">
+      <header className="text-center mb-2 mx-auto bg-blue-gray py-4">
+        <h1 className="text-3xl text-lite-green">
+          Ja<span className="text-neutral-300">mmm</span>ing
+        </h1>
+      </header>
+      <SearchBar />
+      <Playlist />
+      <Tracklist />
+    </div>
   );
 }
 
