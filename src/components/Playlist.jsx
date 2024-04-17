7598790e0f6d0a33372c162b6a202643f7d269da
Dan McGrath <mcgrathd3254@yahoo.com>
2024-04-16 20:10:49 -0400

    Style playlist component

---
+++ src/components/Playlist.jsx
@@ -0,0 +1,23 @@
+import Tracklist from "./Tracklist";
+
+const Playlist = () => {
+  return (
+    <div className="border-dark-green border">
+      <h2 className="text-blue text-2xl text-center font-semibold my-4">
+        Playlist
+      </h2>
+      <input
+        type="text"
+        placeholder="Playlist Title"
+        className="w-5/6 border-none bg-background text-lite-green text-lg focus-visible:outline-none font-semibold mx-auto block placeholder-dark-green"
+      />
+      <hr className="w-5/6 mx-auto" />
+      <Tracklist />
+      <button className="w-1/2 mx-auto bg-blue rounded-md block p-4 text-lite-green font-semibold">
+        Save to Spotify
+      </button>
+    </div>
+  );
+};
+
+export default Playlist;
