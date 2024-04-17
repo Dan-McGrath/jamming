9e7e00bf8f465e117b09bbc66ffe5244e9e1ce11
Dan McGrath <mcgrathd3254@yahoo.com>
2024-04-16 21:18:32 -0400

    Update styling

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -2,8 +2,8 @@ import Tracklist from "./Tracklist";
 
 const Playlist = () => {
   return (
-    <div className="border-dark-green border">
-      <h2 className="text-blue text-2xl text-center font-semibold my-4">
+    <div className="border-dark-green border rounded-md">
+      <h2 className="text-blue text-4xl text-center font-semibold my-4">
         Playlist
       </h2>
       <input
@@ -11,7 +11,7 @@ const Playlist = () => {
         placeholder="Playlist Title"
         className="w-5/6 border-none bg-background text-lite-green text-lg focus-visible:outline-none font-semibold mx-auto block placeholder-dark-green"
       />
-      <hr className="w-5/6 mx-auto" />
+      <hr className="w-5/6 mx-auto border-white/30" />
       <Tracklist />
       <button className="w-1/2 mx-auto bg-blue rounded-md block p-4 text-lite-green font-semibold">
         Save to Spotify
