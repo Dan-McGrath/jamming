274601194d926c79c0df8c06830f3c432fa91d3e
Dan McGrath <mcgrathd3254@yahoo.com>
2024-04-16 21:18:22 -0400

    create mock array and add basic props to child components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,50 @@
 import Playlist from "./components/Playlist";
 import SearchBar from "./components/SearchBar";
 import SearchResults from "./components/SearchResults";
-
+import { useState } from "react";
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [playlist, setPlaylist] = useState([]);
+
+  // Mock Arr
+  const mockArr = [
+    {
+      song: "Song 1",
+      artist: "Artist 1",
+      album: "Album 1",
+    },
+    {
+      song: "Song 2",
+      artist: "Artist 2",
+      album: "Album 3",
+    },
+    {
+      song: "Song 3",
+      artist: "Artist 1",
+      album: "Album 2",
+    },
+    {
+      song: "Song 4",
+      artist: "Artist 3",
+      album: "Album 1",
+    },
+  ];
+
+  const handleSearchResults = (string) => {
+    setSearchResults(mockArr.filter((element) => element === string));
+  };
+
   return (
     <div className="min-h-lvh">
       <header className="text-center mb-2 mx-auto bg-blue-gray py-4">
-        <h1 className="text-3xl text-lite-green">
+        <h1 className="text-6xl text-lite-green">
           Ja<span className="text-neutral-300">mmm</span>ing
         </h1>
       </header>
-      <SearchBar />
-      <div className="grid grid-cols-2 w-10/12 mx-auto gap-6 min-h-lvh">
-        <SearchResults />
-        <Playlist />
+      <SearchBar onSearch={handleSearchResults} />
+      <div className="grid grid-cols-2 w-10/12 mx-auto gap-6 min-h-lvh rounded-md">
+        <SearchResults searchResults={searchResults} />
+        <Playlist playlist={playlist} />
       </div>
     </div>
   );
