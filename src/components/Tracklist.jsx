37dcf57bd2617d7bbaaa33dc876e6d707cd21376
Dan McGrath <mcgrathd3254@yahoo.com>
2024-04-16 22:21:48 -0400

    drill porps down to child component

--- src/components/Tracklist.jsx
+++ src/components/Tracklist.jsx
@@ -1,8 +1,8 @@
 import Track from "./Track";
-const Tracklist = () => {
+const Tracklist = ({ songs, playlist }) => {
   return (
     <div className="my-4 mx-3">
-      <Track />
+      <Track songs={songs} playlist={playlist} />
     </div>
   );
 };
