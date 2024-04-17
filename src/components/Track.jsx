72e581034af3773fe3c870d45ef240215fd1d8aa
Dan McGrath <mcgrathd3254@yahoo.com>
2024-04-16 21:19:24 -0400

    add mock info and create styling

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -1,5 +1,21 @@
 const Track = () => {
-  return <div>Track</div>;
+  return (
+    <>
+      <div className="grid grid-cols-12 items-center py-3">
+        <div className="col-span-10">
+          <h3 className="text-lite-green text-lg">Song Name</h3>
+          <p className="text-dark-green/80">Artist</p>
+          <p className="text-dark-green/80">Album</p>
+        </div>
+        <div className="col-start-11">
+          <button className="rounded-full bg-blue text-lite-green p-1 font-bold">
+            +/-
+          </button>
+        </div>
+      </div>
+      <hr />
+    </>
+  );
 };
 
 export default Track;
