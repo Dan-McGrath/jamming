7171de47cbc8933451cb77d97872c946b928a43d
Dan McGrath <mcgrathd3254@yahoo.com>
2024-04-16 22:21:38 -0400

    add ternary statement for search results

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -1,17 +1,38 @@
-const Track = () => {
+const Track = ({ songs, playlist }) => {
   return (
     <>
       <div className="grid grid-cols-12 items-center py-3">
-        <div className="col-span-10">
-          <h3 className="text-lite-green text-lg">Song Name</h3>
-          <p className="text-dark-green/80">Artist</p>
-          <p className="text-dark-green/80">Album</p>
-        </div>
-        <div className="col-start-11">
-          <button className="rounded-full bg-blue text-lite-green p-1 font-bold">
-            +/-
-          </button>
-        </div>
+        {songs &&
+          songs.map((song, i) => (
+            <>
+              <div key={i} className="col-span-10">
+                <div>
+                  <h3 className="text-lite-green text-lg">{song.song}</h3>
+                  <p className="text-dark-green/80">{song.artist}</p>
+                  <p className="text-dark-green/80">{song.album}</p>
+                </div>
+              </div>
+              <div className="col-start-11">
+                {playlist.filter(function (obj) {
+                  return Object.keys(obj).some(function (key) {
+                    return obj[key].includes(song);
+                  });
+                }) ? (
+                  <>
+                    <button className="rounded-full bg-blue text-lite-green p-1 font-bold">
+                      -
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <button className="rounded-full bg-blue text-lite-green p-1 font-bold">
+                      +
+                    </button>
+                  </>
+                )}
+              </div>
+            </>
+          ))}
       </div>
       <hr />
     </>
