0424b97b3f7f74d02442b5add19379a69949531a
Dan McGrath <mcgrathd3254@yahoo.com>
2024-04-16 21:19:00 -0400

    create handle submit and make input and button child of form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,30 @@
-const SearchBar = () => {
+import { useState } from "react";
+const SearchBar = ({ onSearch }) => {
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(input);
+  };
+
   return (
     <div className="mx-auto my-5 max-w-fit">
-      <input
-        className="rounded-md px-2 py-2 placeholder:text-dark-green"
-        type="text"
-        placeholder="Search for songs"
-      />
-      <button className="text-lite-green m-2 rounded-xl bg-blue px-6 py-2 hover:bg-lite-green/80 hover:text-blue hover:font-semibold">
-        Search
-      </button>
+      <form>
+        <input
+          className="rounded-md px-2 py-2 placeholder:text-dark-green text-blue font-semibold text-lg"
+          type="text"
+          name="search"
+          placeholder="Search for songs"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="text-lite-green mx-5 rounded-xl bg-blue px-6 py-2 hover:bg-lite-green/80 hover:text-blue-gray hover:font-semibold text-lg"
+        >
+          Search
+        </button>
+      </form>
     </div>
   );
 };
