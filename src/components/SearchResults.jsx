d76b51200585eee90405772481551059e63701c4
Dan McGrath <mcgrathd3254@yahoo.com>
2024-04-17 20:10:01 -0400

    drill props down

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,12 +1,22 @@
 import Tracklist from "./Tracklist";
-const SearchResults = ({ searchResults, playlist }) => {
+const SearchResults = ({
+  searchResults,
+  playlist,
+  onAddSong,
+  onRemoveSong,
+}) => {
   return (
     <div className="border rounded-md border-dark-green">
       <h2 className="my-4 text-4xl font-semibold text-center text-blue">
         Results
       </h2>
       <div>
-        <Tracklist songs={searchResults} playlist={playlist} />
+        <Tracklist
+          songs={searchResults}
+          playlist={playlist}
+          onAddSong={onAddSong}
+          onRemoveSong={onRemoveSong}
+        />
       </div>
     </div>
   );
